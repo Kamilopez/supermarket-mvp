6031f05ff0ce871c6152151577c9effab57c0f96
Cristian Lopez Giraldo <clopez7374@campuscecep.edu.co>
2024-10-10 22:43:39 -0500

    ProductModel

---
+++ Models/ProductModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+
+
+namespace Supermarket_mvp.Models
+{
+    internal class ProductModel
+    {
+        [DisplayName("Product Id")]
+        public int Id { get; set; }
+
+        [DisplayName("Product Name")]
+        [Required(ErrorMessage = "Product name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Product name must be between 3 and 50 characters")]
+        public string Name { get; set; }
+
+        [DisplayName("Stock")]
+        [Required(ErrorMessage = "Stock is required")]
+        [Range(0, int.MaxValue, ErrorMessage = "Stock must be a positive number")]
+        public int Stock { get; set; }
+
+        [DisplayName("Price")]
+        [Required(ErrorMessage = "Price is required")]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Price must be a positive number greater than zero")]
+        public decimal Price { get; set; }
+
+        [DisplayName("Category Id")]
+        [Required(ErrorMessage = "Category Id is required")]
+        public int Category_Id { get; set; }
+    }
+}
